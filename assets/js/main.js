f640910b7c980c82a35f81635f647591abdec811
DoubleARon96 <>
2023-11-15 20:49:57 +0000

    trying different ways to get the player choice to log

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 console.log("hi just checking if it works");
-let playerChoice = document.getElementByClassName("buttonChoice")
+let playerChoice = document.getElementsByTagName("input");
 let rock = document.getElementById("rock");
 let scissors = document.getElementById("scissors");
 let lizard = document.getElementById("lizard");
 let spock = document.getElementById("spock");
 let paper = document.getElementById("paper");
-console.log(rock,paper, scissors, lizard, spock);
-
+console.log(rock, paper, scissors, lizard, spock,);
 /**computer decides with this code */
+
     let computerChoice= Math.random();
     if (computerChoice < 0.2) {
         computerChoice = ("rock").src = "assets/images/rock.png"
@@ -20,19 +20,9 @@ console.log(rock,paper, scissors, lizard, spock);
     } else if(computerChoice < 0.9){
         computerChoice = ("spock").src = "assets/images/spock.png"
     }
-console.log(computerChoice)
+console.log(computerChoice);
+/**player choice */
+playerChoice.addEventListener("click", function(){
+    console.log(button.value);
+})
 /**this will compare the rules */
-function runGame(gameType){
-  let playervscomputer = function (computerChoice, playerChoice){
-    if (computerChoice === playerChoice){
-        return "This is a Draw"
-    };
-function checkWinner(){}
-
-function winningHand(){}
-
-function incrementScore(){}
-
-function incrementLosses(){}
-
-function displayChoice(){}
