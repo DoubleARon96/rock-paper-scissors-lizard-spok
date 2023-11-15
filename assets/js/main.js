cb670f135be8a8b8ff1d8ecf4e57ccb26bc6bc5a
DoubleARon96 <>
2023-11-15 20:15:05 +0000

    starting to make player choice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 console.log("hi just checking if it works");
+let playerChoice = document.getElementByClassName("buttonChoice")
 let rock = document.getElementById("rock");
 let scissors = document.getElementById("scissors");
 let lizard = document.getElementById("lizard");
@@ -6,19 +7,26 @@ let spock = document.getElementById("spock");
 let paper = document.getElementById("paper");
 console.log(rock,paper, scissors, lizard, spock);
 
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-    for (let buttons of buttons){
-        buttons.addEventListener("click" function() {
-            
-        })
-
+/**computer decides with this code */
+    let computerChoice= Math.random();
+    if (computerChoice < 0.2) {
+        computerChoice = ("rock").src = "assets/images/rock.png"
+    }else if(computerChoice < 0.3){
+        computerChoice = ("paper").src = "assets/images/paper.png"
+    }else if(computerChoice < 0.5){
+        computerChoice = ("scissors").src = "assets/images/scissors.png"
+    }else if(computerChoice < 0.7){
+        computerChoice = ("lizard").src = "assets/images/lizard.png"
+    } else if(computerChoice < 0.9){
+        computerChoice = ("spock").src = "assets/images/spock.png"
     }
-
+console.log(computerChoice)
+/**this will compare the rules */
 function runGame(gameType){
-    
-}
-
+  let playervscomputer = function (computerChoice, playerChoice){
+    if (computerChoice === playerChoice){
+        return "This is a Draw"
+    };
 function checkWinner(){}
 
 function winningHand(){}
