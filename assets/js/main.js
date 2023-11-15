75369c2eb76d4d6c093021efd138749fa0375442
DoubleARon96 <>
2023-11-15 18:23:25 +0000

    js started added all values to each choice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-console.log("hi just checking if it works")
+console.log("hi just checking if it works");
+let rock = document.getElementById("rock");
+let scissors = document.getElementById("scissors");
+let lizard = document.getElementById("lizard");
+let spock = document.getElementById("spock");
+let paper = document.getElementById("paper");
+console.log(rock,paper, scissors, lizard, spock);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let buttons of buttons){
+        buttons.addEventListener("click" function() {
+            
+        })
+
+    }
+
+function runGame(gameType){
+    
+}
+
+function checkWinner(){}
+
+function winningHand(){}
+
+function incrementScore(){}
+
+function incrementLosses(){}
+
+function displayChoice(){}
