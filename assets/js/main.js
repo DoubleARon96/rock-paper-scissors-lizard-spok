a1c7e012d32b2184012fdb1636ca24e833f293bd
DoubleARon96 <>
2023-11-18 13:22:56 +0000

    i have added a function to add a move count

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,41 @@
 console.log("hi just checking if it works");
 /**the variables are being set here */
 let playerChoice = document.getElementsByClassName("buttonChoice").button;
+/**function for game  */
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
 
+    let playGame = () => {
+        let rock = document.querySelector('#rock');
+        let paper = document.querySelector('#paper');
+        let scissors = document.querySelector('#scissors');
+        let lizard = document.querySelector('#lizard');
+        let spock = document.querySelector('#spock');
+        let playerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+        let computerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+        playerOptions.forEach(option => {
+            option.addEventListener('click' ,function(){
+                let movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerHTML = 'Moves Left :${10-moves}';
+            }
+        )}
+    )}
+}
 /**computer decides with this code */
-let computerChoice = Math.random();
-if (computerChoice < 0.2) {
+let computerChoice = Math.random()*5;
+if (computerChoice < 1) {
     computerChoice = ("rock").img = "assets/images/rock.png";
-} else if (computerChoice < 0.3) {
+} else if (computerChoice < 2) {
     computerChoice = ("paper").img = "assets/images/paper.png";
-} else if (computerChoice < 0.5) {
+} else if (computerChoice < 3) {
     computerChoice = ("scissors").img = "assets/images/scissors.png";
-} else if (computerChoice < 0.7) {
+} else if (computerChoice < 4) {
     computerChoice = ("lizard").img = "assets/images/lizard.png";
-} else if (computerChoice < 0.9) {
+} else if (computerChoice < 5) {
     computerChoice = ("spock").img = "assets/images/spock.png";
 } else {
     computerChoice = "Computer Is Thinking";
@@ -82,3 +105,4 @@ let comPare ={
         "spock": "draw"
     },
 }
+/**win loose */
