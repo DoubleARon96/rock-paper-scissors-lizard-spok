1edd87869fde8591343fc0ab94e60f24b79886f3
DoubleARon96 <>
2023-11-20 22:23:14 +0000

    the win count and loss count go up with the correct out comes on html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
-console.log("hi just checking if it works"); 
-let playerChoice= document.getElementsByClassName ("buttonChoice")
-console.log(playerChoice)
+console.log("hi just checking if it works");
+let playerChoice = document.getElementsByClassName("buttonChoice");
+console.log(playerChoice);
 
 /**global variables*/
 let playerScore = 0;
 let computerScore = 0;
-let moves = 0;
+let movesLeft = 0;
+let playerScoreElement = document.getElementById("playerScore");
+let computerScoreElement = document.getElementById("computerScore");
+let movesLeftElement = document.getElementById("movesLeft");
 /**end of global variables*/
 
 /**function for game*/
@@ -20,47 +23,48 @@ let game = () => {
         let computerOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
         playerOptions.forEach(option => {
-            option.addEventListener('click' ,function(){
+            option.addEventListener('click', function () {
                 let movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerHTML = 'Moves Left :${10-moves}';
 
                 /**function to see who wins */
-                winner(this.innerText.computerChoice)
+                winner(this.innerText.computerChoice);
                 /**the game over count  */
-                if(moves === 10){
-                    gameOver(playerOptions.movesLeft)
+                if (moves === 10) {
+                    gameOver(playerOptions.movesLeft);
                 }
             }
-        )}
-    )}
-}
-;
+            );
+        }
+        );
+    };
+};
 
 /**player choice */
 function changeImageBackground(option) {
     let imgUrl = "assets/images/rock-paper-lizard-spock.png";
-    switch(option){
+    switch (option) {
         case 'rock': imgUrl = 'assets/images/rock.png';
-        playerChoice= "rock"
-        break;
-        case 'paper': imgUrl = 'assets/images/paper.png'
-        playerChoice = "paper"
-        break;
-        case 'scissors': imgUrl = 'assets/images/scissors.png'
-        playerChoice = "scissors"
-        break;
+            playerChoice = "rock";
+            break;
+        case 'paper': imgUrl = 'assets/images/paper.png';
+            playerChoice = "paper";
+            break;
+        case 'scissors': imgUrl = 'assets/images/scissors.png';
+            playerChoice = "scissors";
+            break;
         case 'lizard': imgUrl = 'assets/images/lizard.png';
-        playerChoice = "lizard"
+            playerChoice = "lizard";
             break;
         case 'spock': imgUrl = 'assets/images/spock.png';
-        playerChoice = "spock"
+            playerChoice = "spock";
             break;
-            default: alert(console.error("somthing is wrong "))
-        };
-        console.log(playerChoice)
-        let imageElement = document.getElementById("picture1");
-        imageElement.src= imgUrl;
+        default: alert(console.error("something is wrong "));
+    };
+    console.log(playerChoice);
+    let imageElement = document.getElementById("picture1");
+    imageElement.src = imgUrl;
     /**computer decides with this code */
     let computerChoice = Math.random() * 5;
     if (computerChoice < 1) {
@@ -78,46 +82,56 @@ function changeImageBackground(option) {
     }
     console.log(computerChoice.name);
     let comPuter = document.getElementById("comPuter");
-    comPuter.setAttribute("src", computerChoice.img)
-    
- 
-/**Rules */
-switch (`${playerChoice}:${computerChoice.name}`) {
-    case "rock:rock":
-    case "paper:paper":
-    case "scissors:scissors":
-    case "lizard:lizard":
-    case "spock:spock":
-        console.log("It's a tie!");
-        alert("You Tie!")
-        break;
-    case "scissors:rock":
-    case "lizard:rock":
-    case "spock:paper":
-    case "rock:paper":
-    case "paper:scissors":
-    case "lizard:scissors":
-    case "spock:lizard":
-    case "paper:lizard":
-    case "scissors:spock":
-    case "rock:spock":
-        computerScore ++
-        console.log("You lose!");
-        alert("You Lose!")
-        break;
-    case "rock:scissors":
-    case "rock:lizard":
-    case "paper:spock":
-    case "paper:rock":
-    case "scissors:paper":
-    case "scissors:lizard":
-    case "lizard:spock":
-    case "lizard:paper":
-    case "spock:scissors":
-    case "spock:rock":
-        playerScore ++
-        console.log("You win!");
-        alert("You Win!")
-        break;
-}}
+    comPuter.setAttribute("src", computerChoice.img);
+
+
+    /**Rules */
+    switch (`${playerChoice}:${computerChoice.name}`) {
+        case "rock:rock":
+        case "paper:paper":
+        case "scissors:scissors":
+        case "lizard:lizard":
+        case "spock:spock":
+            console.log("It's a tie!");
+            alert("You Tie!");
+            break;
+        case "scissors:rock":
+        case "lizard:rock":
+        case "spock:paper":
+        case "rock:paper":
+        case "paper:scissors":
+        case "lizard:scissors":
+        case "spock:lizard":
+        case "paper:lizard":
+        case "scissors:spock":
+        case "rock:spock":
+            computerScore++;
+            console.log("You lose!");
+            alert("You Lose!");
+            break;
+        case "rock:scissors":
+        case "rock:lizard":
+        case "paper:spock":
+        case "paper:rock":
+        case "scissors:paper":
+        case "scissors:lizard":
+        case "lizard:spock":
+        case "lizard:paper":
+        case "spock:scissors":
+        case "spock:rock":
+            playerScore++;
+            console.log("You win!");
+            alert("You Win!");
+            break;
+    }
+    playerScoreElement.innerHTML = playerScore;
+    computerScoreElement.innerHTML = computerScore;
+    movesLeftElement.innerHTML = movesLeft;
+
+
+
+
+
+
+}
 /**win loose */
